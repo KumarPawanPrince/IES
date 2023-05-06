25a232e1399aacf327c7446a8573580f6f5e92f1
pawan <navodayag7@gmail.com>
2023-05-06 13:44:34 +0530

    CR-122 Changes

--- Demo.java
+++ Demo.java
@@ -2,5 +2,9 @@ public class Demo{
 
 public static void main(String...args){
 System.out.println("Hi");
+
 }
+//For CR-122
+public void doProcess(){
+       //logic thinking but not completed yet
 }
